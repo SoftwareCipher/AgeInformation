f88d6a865b2cabb92ab816f7bce63702429fa814
Observer <kirillkuznetsov181@gmail.com>
2023-01-29 19:28:16 +0200

    start project

---
+++ src/main/java/com/project/information/AgeInformationApplication.java
@@ -0,0 +1,13 @@
+package com.project.information;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgeInformationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgeInformationApplication.class, args);
+    }
+
+}
